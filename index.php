3ce9d8349c66c3f15af4ba1d082070860c47b24b
joris <jorismartin3@gmail.com>
2025-01-28 17:51:11 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Blind Test</title>
+</head>
+</html>
+
+<body>
+    <?= "COUCOU" ?>
+</body>
+
+<script src="script.js"></script>
