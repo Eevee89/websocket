a03dcb964faa831b7e495cd9001bcde2060fc3c2
joris <jorismartin3@gmail.com>
2025-01-29 22:19:08 +0100

    CSS + roomInput

--- mobile.php
+++ mobile.php
@@ -1,7 +1,8 @@
 <body>
     <div id="connBody" class="fakeBody">
         <div id="connBox" class="box">
-            <input type="text" id="pseudoInput" name="name"placeholder="Entrez votre pseudo"/>
+            <input type="text" id="pseudoInput" name="pseudo" placeholder="Entrez votre pseudo"/>
+            <input type="text" id="roomInput" name="room" placeholder="ID de la partie"/>
             <button id="pseudoSubmit">Rejoindre</button>
         </div>
     </div>
