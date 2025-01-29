07fda92d1af3d0f00805e8fc09a4359a15190646
joris <jorismartin3@gmail.com>
2025-01-29 23:29:37 +0100

    Add label

--- mobile.php
+++ mobile.php
@@ -5,9 +5,8 @@
             <input type="number" id="roomInput" name="room" placeholder="ID de la partie" min="10000" max="99999"/>
             <button id="pseudoSubmit">Rejoindre</button>
         </div>
+        <p id="notroom">La partie n'existe pas</p>
     </div>
-
-    <p id="notroom">La partie n'existe pas</p>
 </body>
 
 <script src="mobile.js"></script>
