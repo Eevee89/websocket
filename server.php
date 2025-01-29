fb9d3d066c020df7210cee8522da4ab500b4e47b
joris <jorismartin3@gmail.com>
2025-01-29 22:32:58 +0100

    Add log

--- server.php
+++ server.php
@@ -106,6 +106,7 @@ class ServerImpl implements MessageComponentInterface {
                     "payload" => ""
                 ];
                 $conn->send(json_encode($res));
+                logMessage(sprintf("New message sent to '%s': %s", $client->resourceId, $raw));
             }
         }
     }
