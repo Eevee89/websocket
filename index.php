03eae1e3fd65ee381cfa0a8427d2c9075f7f9832
joris <jorismartin3@gmail.com>
2025-02-04 23:47:37 +0100

    Add conn page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
 $isMob = is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile"));
@@ -23,10 +24,35 @@ $isios = stripos($userAgent, 'iPhone') !== false || stripos($userAgent, 'iPad') 
 </head>
 </html>
 
-<?php 
+<?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+    <?php if ($_POST["action"] === "Create room"): ?>
+        <?= include "php/master.php"; ?>
+    <?php else: ?>
+        <?php if ($_POST["action"] === "Join room"): ?>
+            <?= include "php/player.php"; ?>
+        <?php else: ?>
+            <div id="connBody" class="fakeBody">
+                <form id="connBox" class="box" method="POST" action="">
+                    <input type="text" id="pseudoInput" name="pseudo" placeholder="Entrez votre pseudo"/>
+                    <input type="submit" name="action" id="joinRoom" value="Join room">Rejoindre</button>
+                </form>
+                <div id="sepconn" style="width: 1px; height: 20px;"></div>
+                <button id="rules">Comment jouer ?</button>
+            </div>
+        <?php endif; ?>
+    <?php endif; ?>
 
-if (!$isMob) {
-    include "php/player.php";
-} else {
-    include "php/master.php";
-}
+<?php else: ?>
+    <div id="connBody" class="fakeBody">
+        <p id="notconnected" style="color: red;"> Erreur de connexion au serveur </p>
+        <form id="connBox" class="box" method="POST" action="">
+            <input type="text" id="pseudoInput" name="pseudo" placeholder="Entrez votre pseudo"/>
+            <div id="connBtns">
+                <input type="submit" name="action" id="createRoom" value="Create room">Créer</button> 
+                <input type="submit" name="action" id="joinRoom" value="Join room">Rejoindre</button>
+            </div>
+        </form>
+        <div id="sepconn" style="width: 1px; height: 20px;"></div>
+        <button id="rules">Comment jouer ?</button>
+    </div>
+<?php endif; ?>
